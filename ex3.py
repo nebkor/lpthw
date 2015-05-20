2f6206d23b42b326b469b91a6a5c7ac45e0cd67f
Richard Casey <wabkia@gmail.com>
2015-05-19 21:46:00 -0700

    added comments per study drill 1 on exercise 3

--- ex3.py
+++ ex3.py
@@ -1,23 +1,29 @@
+# The line below prints the string "I will now count my chickens:" followed by a new line
 print "I will now count my chickens:"
-
+# evalutates 25 + (30 / 6) then prints the string "Hens" followed by a space 
+# followed by the result of the arithmatic expression
 print "Hens", 25 + 30 / 6
+# 100 - ((25 * 3) % 4) -> 100 - (75 % 4) -> 100 - 3 -> 97
 print "Roosters", 100 - 25 * 3 % 4
 
 print "Now I will count the eggs:"
-
+# (3 + 2 + 1 - 5) + (4 % 2) - (1 / 4) + 6
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
-
+# Prints the string
 print "Is it true that 3 + 2 < 5 - 7?"
-
+# (3 + 2) < (5 - 7) -> 5 < -2 -> False
 print 3 + 2 < 5 - 7
-
+# The line below prints "what is 3 + 2?" followed by a space followed by a 5
 print "What is 3 + 2?", 3 + 2
+# prints "what is 5 -7?" followed by a space followed by -2
 print "What is 5 - 7?", 5 - 7
 
 print "Oh, that's why it's False"
-
+# 
 print "How about some more."
-
+# Prints "Is it greater?" followed by a space followed by True
 print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5>= -2
+# prints "Is it greater or equal?" followed by a space followed by True
+print "Is it greater or equal?", 5 >= -2
+# prints "Is it less or equal?" followed by a space followed by False
 print "Is it less or equal?", 5 <= -2
