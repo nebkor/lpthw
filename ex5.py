9a186fa4c8b4669f3547510ff5b7078d3e01c346
Richard Casey <wabkia@gmail.com>
2015-05-21 22:45:57 -0700

    completed study drills 3 & 4 for ex5

--- ex5.py
+++ ex5.py
@@ -5,10 +5,12 @@ weight = 180 # lbs
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
+intocm = 2.54
+lbtokg = 1.0 / 2.2
 
 print "Let's talk about %s." % name
-print "He's %d inches tall." % height
-print "He's %d pounds heavy." % weight
+print "He's %d centimeters tall." % (height * intocm)
+print "He's %d kilograms heavy." % (weight * lbtokg)
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee" % teeth
@@ -16,3 +18,4 @@ print "His teeth are usually %s depending on the coffee" % teeth
 # This line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d" % (
 	age, height, weight, age + height + weight)
+
