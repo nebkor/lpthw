e69ca7059b226ca5d914e21aa544a8a053234281
Joe Ardent <code@ardent.nebcorp.com>
2015-05-21 23:16:52 -0700

    explanatory comments inserted.

--- joe/ex6.py
+++ joe/ex6.py
@@ -1,3 +1,4 @@
+# x will have the string value "There are 10 types of people."
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
@@ -10,11 +11,25 @@ print "I said %r." % x
 print "I also said '%s'." % y
 
 hilarious = True
+
+# the variable 'joke_evaluation' will have the literal value,
+# "Isn't that joke so funny!? %r".
 joke_evaluation = "Isn't that joke so funny!? %r"
 
-print joke_evaluation % hilarious
+# the variable 'evaluated_joke' will have the value,
+# "Isn't that joke so funny!? True" because the '%r'
+# in the joke_evaluation string will have been
+# interpolated with the "raw" representation of the
+# boolean "True".
+evaluated_joke = joke_evaluation % hilarious
+
+# now print that evaluated string
+print evaluated_joke
 
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# the use of the '+' operator here is called
+# "operator overloading". here that character is
+# overloaded to mean string concatenation.
 print w + e
