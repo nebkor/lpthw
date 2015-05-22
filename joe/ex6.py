561a86ea6557f2b543084d59ea9f4895348e5692
Joe Ardent <code@ardent.nebcorp.com>
2015-05-21 23:24:47 -0700

    Better comments.

--- joe/ex6.py
+++ joe/ex6.py
@@ -7,29 +7,33 @@ y = "Those who know %s and those who %s." % (binary, do_not)
 print x
 print y
 
+# This prints out, without the outer double quotes,
+# "I said 'There are 10 types of people.'"
+# That means that the raw string representation of a string
+# gets single-quoted.
 print "I said %r." % x
 print "I also said '%s'." % y
 
 hilarious = True
 
-# the variable 'joke_evaluation' will have the literal value,
+# The variable 'joke_evaluation' will have the literal value,
 # "Isn't that joke so funny!? %r".
 joke_evaluation = "Isn't that joke so funny!? %r"
 
-# the variable 'evaluated_joke' will have the value,
+# The variable 'evaluated_joke' will have the value,
 # "Isn't that joke so funny!? True" because the '%r'
 # in the joke_evaluation string will have been
 # interpolated with the "raw" representation of the
 # boolean "True".
 evaluated_joke = joke_evaluation % hilarious
 
-# now print that evaluated string
+# Now print that evaluated string.
 print evaluated_joke
 
 w = "This is the left side of..."
 e = "a string with a right side."
 
-# the use of the '+' operator here is called
-# "operator overloading". here that character is
+# The use of the '+' operator here is called
+# "operator overloading". Here, that character is
 # overloaded to mean string concatenation.
 print w + e
