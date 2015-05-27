9bfdb2368fa4f339eb5325a14ccf16688553df83
Richard Casey <wabkia@gmail.com>
2015-05-27 12:05:40 -0700

    Exercise 7 completed. Even did a double check and found an error I had previously missed. so responsible.

---
+++ ex7.py
@@ -0,0 +1,37 @@
+# prints "Mary had a little lamb."
+print "Mary had a little lamb."
+# prints "Its fleece was white as %s" with the variable snow
+# and incorrect grammar
+print "Its fleece was white as %s" % 'snow'
+# prints "And everywhere that Mary went."
+print "And everywhere that Mary went."
+# prints ..........
+print "." * 10 # what'd that do # it prints the string 10 times
+				# without spaces
+# sets the variable end(1-10) to c h e e s e b u r g e r, respectively
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try moving it and see what happens
+# the comma combines the two print lines with a space, cheese burger
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# cheese
+# burger
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# I love cheeseburgers.
+# Mistake log:
+# I didn't capitolize C and B for Cheese Burger.... Will that get fixed in UI? :P
