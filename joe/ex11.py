e7446cb6b2bb0111bab6d0e3eb829ff4e2dc9889
Joe Ardent <code@ardent.nebcorp.com>
2015-05-28 23:08:05 -0700

    Use the prompt argument to raw_input instead of an explicit 'print'

--- joe/ex11.py
+++ joe/ex11.py
@@ -1,9 +1,6 @@
-print "How old are you?",
-age = raw_input()
-print "How tall are you?",
-height = raw_input()
-print "How much do you weigh?",
-weight = raw_input()
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
 
 print "So you're %r old, %r tall, and %r heavy." % (
     age, height, weight)
