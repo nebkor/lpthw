46a3855050409d35993287a181bf124e6761b14d
Richard Casey <wabkia@gmail.com>
2015-05-28 22:31:58 -0700

    Added spinner

--- ex10.py
+++ ex10.py
@@ -2,15 +2,25 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+'''
 
 print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
 
+# A tiny piece of code to try out from the book
+while True:
+	for i in ["/", "-", "|", "\\", "|"]:
+		print "%s\r" % i,
+
+# note s for later CAPITOLIZE T IN TRUE
+# make sure to commit when the original 
+# exercise is complete, before adding anything
+
+# NEVER FORGET YOUR PROPER PUNCTUATION.
