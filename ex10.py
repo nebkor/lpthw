69b9fc2234d8fea37bea95c7a4ad4bd0810b4696
Richard Casey <wabkia@gmail.com>
2015-05-28 22:50:57 -0700

    Completed study drills for ex10

--- ex10.py
+++ ex10.py
@@ -14,13 +14,21 @@ print persian_cat
 print backslash_cat
 print fat_cat
 
+# playing with more complex formatting per study drill 3
+print "%s\n%s\"%s\"%s" % (tabby_cat, persian_cat, backslash_cat, fat_cat)
+# Using %r to see the demonstrate the differences between the string format
+# and the raw format
+print "%r\n%r\"%r\"%r" % (tabby_cat, persian_cat, backslash_cat, fat_cat)
+
+
 # A tiny piece of code to try out from the book
-while True:
-	for i in ["/", "-", "|", "\\", "|"]:
-		print "%s\r" % i,
+#while True:
+#	for i in ["/", "-", "|", "\\", "|"]:
+#		print "%s\r" % i,
 
 # note s for later CAPITOLIZE T IN TRUE
 # make sure to commit when the original 
 # exercise is complete, before adding anything
 
-# NEVER FORGET YOUR PROPER PUNCTUATION.
+# NEVER FORGET YOUR PROPER PUNCTUATION. 
+# We wasted a good 5 minutes trying to fix a non existant issue
