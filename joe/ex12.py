0aa9fb15e63694c59bb62e2045270cab0f70f572
Joe Ardent <code@ardent.nebcorp.com>
2015-06-02 21:12:11 -0700

    Calculate required mass/year accumulation.

--- joe/ex12.py
+++ joe/ex12.py
@@ -1,6 +1,13 @@
-age = raw_input("How old are you? ")
+age = float(raw_input("How old are you? "))
 height = raw_input("How tall are you? ")
-weight = raw_input("How much do you weigh? ")
+weight = float(raw_input("How much do you weigh? "))
 
 print "So you're %r old, %r tall, and %r heavy." % (
     age, height, weight)
+
+print
+
+print "If you gained weight at a constant rate, you",
+print "would have had to have gained"
+print "%d mass-units per year " % (weight/age),
+print "to have achieved your current stature."
