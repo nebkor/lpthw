9d2ba9fe5272d334c394e55474fc8161eb1d2b33
Richard Casey <wabkia@gmail.com>
2015-06-04 21:42:31 -0700

    Exercise 14, all study drills complete

--- ex14.py
+++ ex14.py
@@ -1,11 +1,11 @@
 from sys import argv
 
-script, user_name = argv
+script, user_name, mood = argv
 prompt = '* '
 
 print "Hi %s, I'm the %s script." % (user_name, script)
-print "I'd like to ask you a few questions."
-print "Do you like me %s?" % user name
+print "I see youre in a %s mood, but I'd like to ask you a few questions." % mood
+print "Do you like me %s?" % user_name
 likes = raw_input(prompt)
 
 print "Where do you live %s?" % user_name
@@ -14,8 +14,12 @@ lives = raw_input(prompt)
 print "What kind of computer do you have?"
 computer = raw_input(prompt)
 
+print "What room of the house is your computer in?"
+comp_room = raw_input(prompt)
+
 print """
 Alright, so you said %r about liking me.
 You live in %r. Not sure where that is.
 And you have %r computer. Nice.
-""" % (likes, lives, computer)
+I bet your computer likes being in the %r.
+""" % (likes, lives, computer, comp_room)
