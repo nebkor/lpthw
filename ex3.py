4dda73a2d92bdc7455272e8131f875bbb924be61
Richard Casey <wabkia@gmail.com>
2015-06-09 17:47:15 -0700

    added pizza circumference calculator to ex3

--- ex3.py
+++ ex3.py
@@ -27,3 +27,20 @@ print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 # prints "Is it less or equal?" followed by a space followed by False
 print "Is it less or equal?", 5 <= -2
+
+#Pizza circumference from diameter
+#import sys.arvg into the namespace as argv
+from sys import argv
+
+# Set variable pizza to the second item (first user given argument) in the argv list
+pizza_diameter = argv[1]
+#turn the value of pizza_diameter into an integer
+pizza_diameter_int = int(pizza_diameter)
+# multiply pizza_diameter_int by 3.14 and assign the output to pizza_cir
+pizza_cir = 3.14 * pizza_diameter_int
+
+# prints "If your pizza is" then the value of pizza_diameter followed by
+# "inches diagonaly, then the pizza is" then the value of pizza_cir followed by
+# "inches around!"
+print "If your pizza is %s inches diagonaly, then the pizza is %s around!" % (
+	pizza_diameter, pizza_cir)
