c1809de8c0a1c86032d35e0d9b05f5ecdef67303
Richard Casey <wabkia@gmail.com>
2015-06-09 17:21:07 -0700

    added dict "phrases" and printed their keys and values in a list

--- ex1.py
+++ ex1.py
@@ -1,3 +1,8 @@
+#importing sys.argv into the global namespace as argv
+#from sys import argv
+# assigning args 1-8 to argv
+#args = argv[1:9]
+
 print "Hello World!"
 print "Hello Again"
 #print "I like typing this."
@@ -6,3 +11,10 @@ print 'Yay! Printing.'
 #print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
 print "Oh my god this will totally help in the long run."
+#Creating a dictionary of phrases, indexed by keys 1-8
+phrases = {'1': 'Hello World!','2': 'Hello Again','3': 'I like typing this.','4': 'This is fun.',
+		'5': 'Yay! Printing.','6': 'I\'d much rather you \'not\'.', '7': 'I "said" do not touch this.',
+		'8': 'Oh my god this will totally help me in the long run',}
+# printing all phrases 
+for k, v in phrases.iteritems():
+	print "%s: %s" % (k, v)
