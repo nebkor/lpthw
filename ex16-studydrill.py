293f8bba3bef5e4b5cd7c0ee036f5797eaaf1354
Richard Casey <wabkia@gmail.com>
2015-06-11 14:00:10 -0700

    closed the file in ex16 study drill 2

--- ex16-studydrill.py
+++ ex16-studydrill.py
@@ -5,21 +5,17 @@ from sys import argv
 # the 2nd slice of argv onward
 files_to_be_read = argv[1:]
 
+# Prints "Here are the file(s) you'd like to read:" with a trailing new line
 print "Here are the file(s) you'd like to read:\n"
+# Iterates through the variable files_to_be_read which holds a list 
+# of arguments given to the script then prints the name of the file
+# appended by a ":" followed by a new line and then it's contents before
+# moving to the next entry in the list
 for i in files_to_be_read:
-	print "%s" % i
+	print "%s:\n" % i
+	f = open(i, 'r')
+	print f.read()
 	print
-	print open(i, 'r').readlines()
-
-
-
-
-
-
-
-
-
-
-
+	f.close()
 
 print "This is the end of this script" 
