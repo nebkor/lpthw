f72fe45ccce6827e4fff800a328a979093db27a4
Richard Casey <wabkia@gmail.com>
2015-06-11 14:37:44 -0700

    added comments for ex1 and ex16

--- ex1.py
+++ ex1.py
@@ -5,19 +5,25 @@
 
 print "Hello World!"
 print "Hello Again"
-#print "I like typing this."
+# print "I like typing this."
 print "This is fun."
 print 'Yay! Printing.'
-#print "I'd much rather you 'not'."
+# print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
 print "Oh my god this will totally help in the long run."
-#Creating a dictionary of phrases, indexed by keys 1-8
+# Creating a dictionary of phrases, indexed by keys 1-8
 phrases = {'1': 'Hello World!','2': 'Hello Again','3': 'I like typing this.','4': 'This is fun.',
 		'5': 'Yay! Printing.','6': 'I\'d much rather you \'not\'.', '7': 'I "said" do not touch this.',
 		'8': 'Oh my god this will totally help me in the long run',}
-
+# using the iteritems method on the dictionary held by the variable phrases
+# and assigning k to the key and v to the value of each pair as the for loop
+# iterates through each entry in the dictionary. Then the key is printed appended
+# by a colon and a space followed by the value corresponding to the key
 for k, v in phrases.iteritems():
 	print "%s: %s" % (k, v) 
-
+# creates a list of keys from the dictionary assigned to the variable phrases
+# then sorts it. The for statement iterates through the list of keys assignging
+# i to the output, starting at the first entry, then prints the key appended by :
+# followed by the coresponding value in the phrases dictionary
 for i in sorted(phrases.keys()):
 	print "%s: %s" % (i, phrases[i])
--- ex16.py
+++ ex16.py
@@ -1,30 +1,49 @@
 from sys import argv
-
+# assigning the variable script to argv[0] and the variable
+# filename to argv[1]
 script, filename = argv
 
+# print "We're going to erase" appended by the output of
+# the variable filename
 print "We're going to erase %r" % filename
-print "If you don't want that, hit CTRL-C (^C)."
-print "If you do want that, hit RETURN"
-
+print "If you don't want that, hit CTRL-C (^C)." # printing a string
+print "If you do want that, hit RETURN" # printing a string
+# using the function "raw_input()" to create a stopping point
+# in the script so the user can stop the script before any files
+# are overwritten
 raw_input("?")
 
-print "Opening the file..."
+print "Opening the file..." # printing a string
 
+# assigning the variable target to the writable file object
+# returned by the "open()" function 
 target = open(filename, 'w')
 
-print "Truncating the file. Goodbye!"
+print "Truncating the file. Goodbye!" # printing a string
+# using the "truncate()" method on the output of the target variable
 target.truncate()
 
-print "Now I'm going to ask you for three lines."
+print "Now I'm going to ask you for three lines." # printing a string
 
-line1 = raw_input("line 1: ")
+# assigning the variable line1 to the string input provided by the user
+# and captured by the "raw_input()" function
+line1 = raw_input("line 1: ") 
+# assigning the variable line2 to the string input provided by the user
+# and captured by the "raw_input()" function
 line2 = raw_input("line 2: ")
+# assigning the variable line3 to the string input provided by the user
+# and captured by the "raw_input()" function
 line3 = raw_input("line 3: ")
 
-print "I'm going to write these to the file."
+print "I'm going to write these to the file." # printing a string
 
+# using the "write()" method on the file object held by the variable target
+# to write the string output of line1, line2, and line3, each followed by
+# a new line
 target.write('%s\n%s\n%s' % (line1, line2, line3))
 
 
-print "And finally, we close it."
+print "And finally, we close it." # printing a string
+# using the "close()" method on the file object held by the variable
+# target
 target.close()
