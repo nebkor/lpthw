45f80656b57270c1d5e9662421231db494f2e54f
Richard Casey <wabkia@gmail.com>
2015-06-11 13:32:07 -0700

    working read of all files given as arguments in the ex16 study drill script

--- ex16-studydrill.py
+++ ex16-studydrill.py
@@ -1,9 +1,25 @@
+# Importing sys.argv into the global namespace as argv
 from sys import argv
 
+# assigning the variable files_to_be_read to the list returned from
+# the 2nd slice of argv onward
 files_to_be_read = argv[1:]
 
 print "Here are the file(s) you'd like to read:\n"
 for i in files_to_be_read:
 	print "%s" % i
-print "\n"
+	print
+	print open(i, 'r').readlines()
+
+
+
+
+
+
+
+
+
+
+
+
 print "This is the end of this script" 
