8618f06145d43a6e678af8ed5e3efc764603b560
Richard Casey <wabkia@gmail.com>
2015-06-15 22:17:15 -0700

    exercise 17, study drills complete

--- ex17.py
+++ ex17.py
@@ -1,24 +1,20 @@
-from sys import argv
+from sys import argv, exit
 from os.path import exists
 
-script, from_file, to_file = argv
+from_file, to_file = argv[1:3]
 
-print "Copying from %s to %s" % (from_file, to_file)
-
-# We could do these two in one line, how?
-# A: indata = open(from_file).read()
-in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
 
 print "The input file is %d bytes long" % len(indata)
+try:
+	if exists(to_file) == True:
+		raise IOError("The destination file already esists")
+	else: 
+		open(to_file, 'w').write(indata)
+except IOError as error:
+	print error
+	exit(15)
 
-print "But does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
-
-print "Alright, all done."
-out_file.close()
-in_file.close()
+print "Copied content from %s to %s" % (
+	from_file, to_file)
