cc4abb3276a73fa41e46b143e34cc75babff699c
Joe Ardent <code@ardent.nebcorp.com>
2015-06-16 21:46:33 -0700

    very fancy tricks for printing linenumbers along with the line

--- joe/ex20.py
+++ joe/ex20.py
@@ -8,8 +8,21 @@ def print_all(f):
 def rewind(f):
     f.seek(0)
 
-def print_a_line(line_count, f):
-    print line_count, f.readline()
+def print_a_line(f):
+    print print_a_line.linecount,
+    print f.readline(),
+    print_a_line.linecount += 1
+
+def print_all_lines(f):
+    f.seek(0)
+    current_line = 1
+    try:
+        while f:
+            print "%d %s" % (current_line, f.next()),
+            current_line += 1
+    except StopIteration:
+        pass
+
 
 current_file = open(input_file)
 
@@ -23,11 +36,11 @@ rewind(current_file)
 
 print "Let's print three lines:"
 
-current_line = 1
-print_a_line(current_line, current_file)
+print_a_line.linecount = 1
+print_a_line(current_file)
+print_a_line(current_file)
+print_a_line(current_file)
 
-current_line = current_line + 1
-print_a_line(current_line, current_file)
-
-current_line = current_line + 1
-print_a_line(current_line, current_file)
+print
+print "Now let's print all the lines:"
+print_all_lines(current_file)
