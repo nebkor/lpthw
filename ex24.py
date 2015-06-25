6520fbc031cd74772cafb29ddba0196757ed1749
Richard Casey <wabkia@gmail.com>
2015-06-24 18:39:59 -0700

    Exercise 24, played with code for study drills

--- ex24.py
+++ ex24.py
@@ -21,10 +21,11 @@ def secret_formula(started):
 	jelly_beans = started * 500
 	jars = jelly_beans / 1000
 	crates = jars / 100
-	return jelly_beans, jars, crates
+	booty = "booty"
+	return jelly_beans, jars, crates, booty
 
 start_point = 10000
-beans, jars, crates = secret_formula(start_point)
+beans, jars, crates, string_test = secret_formula(start_point)  
 
 print "With a starting point of: %d" % start_point
 print "We have %d beans, %d jars, and %d crates." % (beans, jars, crates)
@@ -32,4 +33,9 @@ print "We have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do it this way:"
-print "We have %d beans, %d jars, and %d crates." % secret_formula(start_point)
+
+# without assigning booty above, adding a %s to the end of the format string creates a vlaueerror.
+# values are also run through the function in order. Because the string held by booty
+# isn't returned until last, the current implementation of the string print statement 
+# does not work and results in a typeerror
+print "We have %d %s beans, %d jars, and %d crates." % secret_formula(start_point)
