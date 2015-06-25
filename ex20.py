e6e6d13264a19c97efb9233e5307093e89e655b3
Richard Casey <wabkia@gmail.com>
2015-06-24 18:06:40 -0700

    changed some comments in ex20 to reflect the correct information

--- ex20.py
+++ ex20.py
@@ -12,16 +12,16 @@ def print_all(f):
 
 # Define the function "rewind()" which takes an argument as f
 def rewind(f):
-	f.seek(0) # using the .seek() method on a file object to (is this the right way to say this?)
-			  # return the read state of the file back to the 0th byte
+	f.seek(0) # using the .seek() method on a file object to
+			  # return the offset of the file back to the 0th byte
 
 # define the function "print_a_line()" which takes two arguments as
 # line_count and f. 
 def print_a_line(line_count, f):
 	print line_count, f.readline(), # prints the raw value of line_count followed by a space
 									# then uses the .readline() method on the file object f 
-									# to print a string from the file object starting at the current
-									# byte(?) until the next new line
+									# to print a string from the the file object starting at the current
+									# offset until the next new line
 
 # assigning the variable current_file to the output of the "open()" function
 # using the input_file variable as it's argument
@@ -30,15 +30,15 @@ current_file = open(input_file)
 print "First let's print the whole file:\n"
 
 # calls the "print_all()" function on the file object held by the 
-# current_file variable, which performs the .read() method from the object's
-# current state to the end of the file.
+# current_file variable, which performs the .read() method from the 
+# file object's current offset to the end of the file.
 print_all(current_file)
 
 print "Now let's rewind, kind of like a tape."
 
 # calls the "rewind()" function on the file object held by the
 # current_file variable which performs the .seek() method on the file object
-# setting the file state (again,hmm, must be a better way) 
+# setting offset of the file object back to the 0th byte
 rewind(current_file)
 
 print "Now let's print three lines:"
