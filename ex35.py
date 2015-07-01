7bdf1110315dc7161fea8fb783778d572d5e862c
Richard Casey <wabkia@gmail.com>
2015-06-30 23:12:00 -0700

    ex35, not working

--- ex35.py
+++ ex35.py
@@ -1,76 +1,90 @@
 from sys import exit
+import re
+
+# def gold_room():
+#     print "This room is full of gold. How much do you take?"
+
+#     choice = raw_input("> ")
+#     if "O" in choice or "1" in choice:
+#         how_much = int(choice)
+#     else:
+#         dead("Man, learn how to type a number.")
+
+#     if how_much < 50:
+#         print "Nice, you're not greedy, you win!"
+#         exit(0)
+#     else:
+#         dead("You greedy bastard!")
 
 def gold_room():
-	print "This room is full of gold. How much do you take?"
+    print "This room is full of gold. How much do you take?"
 
-	choice = raw_input("> ")
-	if "O" or "1" in choice:
-		how_much = int(choice)
-	else:
-		dead("Man, learn how to type a number.")
+    choice = raw_input("> ")
+    if re.match('[0-9]', choice) and int(choice) < 50:
+        dead("Nice, you're not greedy, you win!")
+    elif int(choice) >= 50:
+        dead("You greedy bastard!")
+    else:
+        dead("Man, learn how to type a number.")
 
-	if how_much < 50:
-		print "Nice, you're not greedy, you win!"
-		exit(0)
-	else:
-		dead("You greedy bastard!")
 
 
 def bear_room():
-	print "There is a bear here."
-	print "The bear has a bunch of honey."
-	print "The fat bear is in front of another door."
-	print "How are you going to move the bear?"
-	bear_moved = False
+    print "There is a bear here."
+    print "The bear has a bunch of honey."
+    print "The fat bear is in front of another door."
+    print "How are you going to move the bear?"
+    bear_moved = False
 
-	while True:
-		choice = raw_input("> ")
+    while True:
+        choice = raw_input("> ")
 
-		if choice == "take honey":
-			dead("The bear looks at you then slaps your face off.")
-		elif choice == "taunt bear" and not bear_moved:
-			print "The bear has moved from the door. You can go through it now"
-			bear_moved = True
-		elif choice == "taunt bear" and bear_moved:
-			dead("The bear gets pissed off and chews your leg off.")
-		elif choice == "open door" and bear_moved:
-			gold_room()
-		else:
-			print "I have no idea what that means."
+        if choice == "take honey":
+            dead("The bear looks at you then slaps your face off.")
+        elif choice == "taunt bear" and not bear_moved:
+            print "The bear has moved from the door. You can go through it now"
+            bear_moved = True
+        elif choice == "taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews your leg off.")
+        elif choice == "open door" and bear_moved:
+            gold_room()
+        else:
+            print "I have no idea what that means."
 
 
 
 def cthulu_room():
-	print "Here you see the great evil Cthulhu."
-	print "He, it, whatever stares at you and you go insane."
-	print "Do you flee for your life or eat your head?"
+    print "Here you see the great evil Cthulhu."
+    print "He, it, whatever stares at you and you go insane."
+    print "Do you flee for your life or eat your head?"
 
-	choice = raw_input("> ")
+    choice = raw_input("> ")
 
-	if "flee" in choice:
-		start()
-	elif "head" in choice:
-		dead("Well that was tasty.")
-	else:
-		cthulu_room()
+    if "flee" in choice:
+        start()
+    elif "head" in choice:
+        dead("Well that was tasty.")
+    else:
+        cthulu_room()
 
 def dead(why):
-	print why, "Good Job!"
-	exit(0)
+    print why, "Good Job!"
+    exit(0)
 
 def start():
-	print "You are in a dark room."
-	print "There is a door to your right and left."
-	print "Which one do you take?"
+    print "You are in a dark room."
+    print "There is a door to your right and left."
+    print "Which one do you take?"
 
-	choice = raw_input("> ")
+    choice = raw_input("> ")
 
-	if choice == "left":
-		bear_room()
-	if choice == "right":
-		cthulu_room()
-	else:
-		dead("You stumble around the room until you starve.")
+    if choice == "left":
+        bear_room()
+    if choice == "right":
+        cthulu_room()
+    else:
+        dead("You stumble around the room until you starve.")
 
 
 start()
+
